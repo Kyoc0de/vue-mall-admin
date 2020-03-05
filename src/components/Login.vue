51f258638ca67cff3eb9ffe9fecf2d85b8b48fd2
kyoo0 <kyoscode@gmail.com>
2020-03-05 13:38:05 -0500

    登录界面基本完成 1.完成验证提示 2.绑定axios请求

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,16 +1,70 @@
 <template>
     <div class="login_container">
         <div class="login_box">
+            <!-- 头像区域 -->
             <div class="avatar_box">
-                <img src="../assets/logo.png" alt="">
+                <img src="../assets/avatar.png" alt="">
             </div>
+            <!-- 登录表单区域 -->
+            <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
+                <!-- 用户名 -->
+                <el-form-item prop="username" >
+                    <el-input v-model="loginForm.username" prefix-icon="el-icon-user-solid" ></el-input>
+                </el-form-item>
+                <!-- 密码 -->
+                 <el-form-item prop="password">
+                    <el-input v-model="loginForm.password" prefix-icon="el-icon-lock" type="password"></el-input>
+                </el-form-item>
+                <!-- 按钮区域 -->
+                 <el-form-item class="btns" >
+                    <el-button type="primary" @click="login">登录</el-button>
+                    <el-button type="info" @click="resetLoginForm">重置</el-button>
+                </el-form-item>
+            </el-form>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data(){
+        return{
+            //这是表单数据绑定对象
+            loginForm:{
+                username: 'admin',
+                password: 'admin'
+            },
+            //这是表单的验证对象
+            loginFormRules:{
+                //验证用户名是否合法
+                username: [ 
+                    { required: true, message: "请输入登录名称", trigger: "blur"},
+                    { min: 3, max: 15, message: "长度在3到15个字符", trigger: "blur"}
+                ],
+                //验证密码是否合法
+                password: [
+                    { required: true, message: "请输入登录密码", trigger: "blur"},
+                    { min: 3, max: 15, message: "长度在3到15个字符", trigger: "blur"}
+                ]
+            }
+        }
+    },
+
+    methods:{
+        resetLoginForm(){
+            this.$refs.loginFormRef.resetFields();
+        },
+        login(){
+            this.$refs.loginFormRef.validate( async(valid)=>{
+                if(!valid) return;
+                const {data: res} = await this.$http.post("login",this.loginForm);
+                if(res.meta.status !== 200)return console.log("登录失败");
+                console.log("登录成功");
+        
+            })
+        }
+
+    }
 }
 </script>
 
@@ -31,8 +85,8 @@ export default {
     transform: translate(-50%,-50%);
 
     .avatar_box{
-        height: 130px;
-        width: 130px;
+        height: 100px;
+        width: 100px;
         border: 1px solid #eee;
         border-radius: 50%;
         padding: 10px;
@@ -47,6 +101,19 @@ export default {
             border-radius: 50%;
             background-color: #eee;
         }
-    }
+    }    
+}
+
+.login_form{
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+
+.btns{
+    display: flex;
+    justify-content: flex-end;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import './plugins/element.js'
 // 导入全局css
 import './assets/css/global.css'
 
+import axios from 'axios'
+axios.defaults.baseURL = "http://127.0.0.1:8080/api/private/v1"
+Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
 
 new Vue({
