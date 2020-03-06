8dbf59fe55c75b413dd4324e903d887d3f33d6a5
kyoo0 <kyoscode@gmail.com>
2020-03-05 23:46:37 -0500

    登录成功后操作将登录成功之后的token,保存到客户端sessionStorage中

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../components/Login.vue'
+import Login from '../components/Login'
+import Home from '../components/Home'
 
 Vue.use(VueRouter)
 
@@ -9,7 +10,8 @@ Vue.use(VueRouter)
 const router = new VueRouter({
   routes:[
     { path: '/',redirect: '/login' },
-    { path: '/login',component: Login }
+    { path: '/login',component: Login },
+    { path: '/home',component: Home }
   ]
 })
 
