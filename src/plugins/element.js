6d948f87977e5c68c06e36acb3f157ffe61c108b
kyoo0 <kyoscode@gmail.com>
2020-03-07 00:36:08 -0500

    首页的基本布局

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -10,12 +10,29 @@ import {
     Form,
     FormItem,
     Input,
-    Message} from 'element-ui'
+    Message,
+    Container,
+    Header,
+    Aside,
+    Main,
+    Menu,
+    Submenu,
+    MenuItemGroup,
+    MenuItem
+} from 'element-ui'
 
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
 //讲弹窗组件挂载到vue上 可通过this.$message访问
 Vue.prototype.$message = Message
