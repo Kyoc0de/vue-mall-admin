f67e1e8ac1009e76e4f4062e92b39e7a9d69bcba
kyoo0 <kyoscode@gmail.com>
2020-03-09 13:31:44 -0400

    权限页面初始化

---
+++ src/components/power/Rights.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+<!--        面包屑-->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+            <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+        </el-breadcrumb>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
