cabe5c3afb155ee32827483d66457d78951cba0b
kyoo0 <kyoscode@gmail.com>
2020-03-09 16:32:07 -0400

    角色列表数据渲染

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -6,12 +6,55 @@
             <el-breadcrumb-item>权限管理</el-breadcrumb-item>
             <el-breadcrumb-item>角色列表</el-breadcrumb-item>
         </el-breadcrumb>
+<!--    卡片视图-->
+        <el-card>
+            <el-row>
+                <el-col>
+                    <el-button type="primary">添加角色</el-button>
+                </el-col>
+            </el-row>
+<!--            角色列表区域-->
+            <el-table :data="rolelist" border stripe>
+<!--                展开列-->
+                <el-table-column type="expand"></el-table-column>
+                <el-table-column type="index" label="#"></el-table-column>
+                <el-table-column label="角色名称" prop="roleName"></el-table-column>
+                <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
+                <el-table-column label="操作" width="300px">
+                    <template >
+<el-button type="primary" size="mini" icon="el-icon-edit">编辑</el-button>
+<el-button type="danger" size="mini" icon="el-icon-delete">删除</el-button>
+<el-button type="warning" size="mini" icon="el-icon-setting">分配权限</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </el-card>
     </div>
 </template>
 
 <script>
     export default {
+        data(){
+            return{
+                //所有角色列表
+                rolelist:[]
+            }
+        },
+        created() {
+            this.getRolesList()
+        },
+        methods:{
+        //    获取所有角色的列表
+            async getRolesList(){
+                const {data:res} = await this.$http.get('roles')
+                if(res.meta.status!==200){
+                    return this.$message.error('获取用户列表失败!')
+                }
+                this.rolelist = res.data
 
+            }
+
+        }
     }
 </script>
 
