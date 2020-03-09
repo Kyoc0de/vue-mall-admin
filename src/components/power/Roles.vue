a252ed9ba84dce584704acb3949da004e8bd3d63
kyoo0 <kyoscode@gmail.com>
2020-03-09 19:52:57 -0400

    角色列表 删除权限 功能

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -17,7 +17,32 @@
 <!--            角色列表区域-->
             <el-table :data="rolelist" border stripe>
 <!--                展开列-->
-                <el-table-column type="expand"></el-table-column>
+                <el-table-column type="expand">
+                    <template slot-scope="scope">
+                        <el-row :class="['bdbottom',i1===0?'bdtop':'','vcenter']" v-for="(item1,i1) in scope.row.children" :key="item1.id" >
+<!--                            一级权限-->
+                            <el-col :span="5">
+                                <el-tag  closable @close="removeRightById(scope.row,item1.id)">{{item1.authName}}</el-tag>
+                                <i class="el-icon-caret-right"></i>
+                            </el-col>
+<!--                            二级和三级-->
+                            <el-col :span="19">
+<!--                                通过for循环嵌套渲染二级权限-->
+                                <el-row :class="[i2===0? '':'bdtop','vcenter']" v-for="(item2,i2) in item1.children" :key="i2">
+                                    <el-col :span="6">
+                                        <el-tag type="success"  closable @close="removeRightById(scope.row,item2.id)">{{item2.authName}}</el-tag>
+                                        <i class="el-icon-caret-right"></i>
+                                    </el-col>
+                                    <el-col :span="18">
+                                        <el-tag type="warning" v-for="(item3 ) in item2.children" :key="item3.id" closable @close="removeRightById(scope.row,item3.id)">
+                                            {{item3.authName}}
+                                        </el-tag>
+                                    </el-col>
+                                </el-row>
+                            </el-col>
+                        </el-row>
+                    </template>
+                </el-table-column>
                 <el-table-column type="index" label="#"></el-table-column>
                 <el-table-column label="角色名称" prop="roleName"></el-table-column>
                 <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
@@ -219,6 +244,26 @@
 
                 this.$message.success('删除用户成功!')
                 this.getRolesList()
+            },
+            //删除权限
+            async removeRightById(role,rightId){
+            //    弹框提示是否删除
+                const confirmResult = await this.$confirm('此操作无法撤销,是否永久删除','提示',{
+                    confirmButtonText:'确定',
+                    cancelButtonText:'取消',
+                    type:'warning'
+                }).catch(err=>err)
+
+                if(confirmResult!=='confirm'){
+                    return this.$message.info('取消了删除!')
+                }
+
+                const { data : res}= await this.$http.delete(`roles/${role.id}/rights/${rightId}`)
+                if(res.meta.status!==200){
+                    return this.$message.error('删除权限失败')
+                }
+
+                role.children = res.data
             }
 
         }
@@ -226,5 +271,18 @@
 </script>
 
 <style lang="less" scoped>
+.el-tag{
+    margin: 7px;
+}
 
+    .bdtop{
+        border-top: 1px solid #eee;
+    }
+    .bdbottom{
+        border-bottom: 1px solid #eee;
+    }
+    .vcenter{
+        display: flex;
+        align-items: center;
+    }
 </style>
